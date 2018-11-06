1be86dc87f4fb74e26df8ea3699d92cfd316e3fa
denism <denis@metrailler.ch>
2018-11-06 20:37:50 +0100

    corrected entry_katest

--- network/dijkstra.py
+++ network/dijkstra.py
@@ -7,10 +7,10 @@ def dijkstra(source, train):
     # 2. Set the distance to zero for our initial node
     # and to infinity for other nodes.
     distances = {vertex.label: -np.inf for vertex in train.network.nodes.values()}
-    #previous_vertices = {vertex.label: None for vertex in train.network.nodes.values()}
+    # previous_vertices = {vertex.label: None for vertex in train.network.nodes.values()}
     vertices = train.network.nodes.copy()
     r = vertices[source].in_links[0].get_requirement()
-    distances[source] = r.get_exit_latest()
+    distances[source] = r.get_exit_latest() - r.get_min_stopping_time()
 
     while vertices:
         # 3. Select the unvisited node with the smallest distance,
@@ -31,22 +31,25 @@ def dijkstra(source, train):
             if r is not None:
                 if r.get_min_stopping_time() is not None:
                     entry_latest = entry_latest - r.get_min_stopping_time()
-                    entry_latest = min(r.get_entry_latest(), entry_latest)
+
+                    latest = r.get_entry_latest()
+                    if latest < 24 * 60 * 60 * 3:
+                        entry_latest = max(r.get_entry_latest(), entry_latest)
 
             # Compare the newly calculated distance to the assigned
             # and save the smaller one.
             if entry_latest > distances[neighbour.label]:
                 distances[neighbour.label] = entry_latest
-                #previous_vertices[neighbour.label] = current_vertex
+                # previous_vertices[neighbour.label] = current_vertex
 
         # 5. Mark the current node as visited
         # and remove it from the unvisited set.
         del vertices[current_vertex]
 
-    #path, current_vertex = deque(), dest
-    #while previous_vertices[current_vertex] is not None:
+    # path, current_vertex = deque(), dest
+    # while previous_vertices[current_vertex] is not None:
     #    path.appendleft(current_vertex)
     #    current_vertex = previous_vertices[current_vertex]
-    #if path:
+    # if path:
     #    path.appendleft(current_vertex)
     return distances
